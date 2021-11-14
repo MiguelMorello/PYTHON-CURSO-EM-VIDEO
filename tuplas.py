7751ce9c81bf6ac9cba213d6879018cc45d80da2
MiguelMorello <93297660+MiguelMorello@users.noreply.github.com>
2021-11-14 17:13:22 -0300

    Tuplas

---
+++ tuplas.py
@@ -0,0 +1,12 @@
+print('-='*19)
+print('Desafio 1')
+print('-='*19)
+cont = ('zero','um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez' )
+while True: 
+	num = int(input('Digite um numero entre 0 e 10:  '))
+	if 0 <= num <= 10:
+		break
+print(f'voce digitou o numero {cont[num]}')
+print('-='*19)
+print('Desafio 2')
+print('-='*19)
